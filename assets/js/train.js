f02fb50731be005d468ab37003a3f9b9ccd32828
Francisco Fernandez <francisco.a.fernandez@gmail.com>
2019-11-30 22:26:13 -0500

    defined variables in JS

---
+++ assets/js/train.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+
+            //Setting Variables
+
+            var destination;
+
+            var nextTrainName;
+
+            var lastTrainName;
+
+            var newTrainTime;
+
+            var lastTrainTime;
+
+            var frequency;
+
+            var minutesAway;
+
+            var nextArrival
+
+            var
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
