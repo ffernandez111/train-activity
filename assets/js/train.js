c3c039a5c3677d6f8769bc237e3701a6362e623d
Francisco Fernandez <francisco.a.fernandez@gmail.com>
2019-11-30 22:34:23 -0500

    pseudocoded some js

--- assets/js/train.js
+++ assets/js/train.js
@@ -19,39 +19,33 @@ $(document).ready(function() {
 
             var nextArrival
 
-            var
 
 
 
 
+            //current time display function (moment.js)
+
+
+            // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
+
+            var database = firebase.database();
 
 
 
+            // Firebase configuration links and api key
 
 
 
+            // Code for uploading new train data to the database
 
 
 
+            // Code for Friebase data row adding in html document
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            //code for submit button new train info search
 
 
         }
